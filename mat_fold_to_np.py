fab862b39d516ca9347ee44fcd0b1a917bee1c4a
doronkopit <doronkopit@mail.tau.ac.il>
2020-10-18 23:23:48 +0300

    tool works on server

--- mat_fold_to_np.py
+++ mat_fold_to_np.py
@@ -1,25 +1,16 @@
 """
-==========================================================
-Load CT slices and plot axial, sagittal and coronal images
-==========================================================
+Converts a folder of dcm images consisting a series of 3d images to numpy arrays
 
-This example illustrates loading multiple files, sorting them by slice
-location, building a 3D image and reslicing it in different planes.
+
 
 .. usage:
 
-   reslice.py <glob>
-   where <glob> refers to a set of DICOM image files.
-
-   Example: python reslice.py "*.dcm". The quotes are needed to protect
-   the glob from your system and leave it for the script.
+   change the variables dir folder path, first img num, last an patient num
 
 .. note:
 
-   Uses numpy and matplotlib.
+   Uses numpy, pydicom and matplotlib.
 
-   Tested using series 2 from here
-   http://www.pcir.org/researchers/54879843_20060101.html
 """
 
 import pydicom
@@ -31,7 +22,7 @@ import glob
 def images_to_3dnump(dir_name, img_num, plot=False):
     # load the DICOM files
     files = []
-    dir_name_for = dir_name + "\\IM-00{:02}*.dcm".format(img_num)
+    dir_name_for = dir_name + "/IM-00{:02}*.dcm".format(img_num)
     print("glob: {}".format(dir_name_for))
     for fname in glob.glob(dir_name_for, recursive=False):
         #       print("loading: {}".format(fname))
@@ -71,7 +62,6 @@ def images_to_3dnump(dir_name, img_num, plot=False):
         img3d[:, :, i] = img2d
 
     return img3d
-    # save to npy file
 
     # plot 3 orthogonal slices
     if plot:
@@ -91,27 +81,25 @@ def images_to_3dnump(dir_name, img_num, plot=False):
 
 
 def convert_matrix_folder_to_3dnpy_files(
-        dir_path, pat_num, first_img, end_img, onefile=True):
+        dir_path,trgt_path, pat_num, first_img, end_img, onefile=True):
     nd_arrs = []
     for img_num in range(first_img, end_img + 1):
         nd_arrs.append(images_to_3dnump(dir_path, img_num))
     if onefile:
-        with open(dir_path + f'mat_{pat_num}_{len(nd_arrs)}.npz', 'w') as out:
+        name=f'mat_{pat_num}_{len(nd_arrs)}_{nd_arrs[0].shape}.npz'
+        with open(trgt_path + name, 'wb') as out:
             np.savez_compressed(out, nd_arrs)
 
     else:
         for i in range(len(nd_arrs)):
-            with open(dir_path + f"{i + 1}.npy", 'w') as out:
+            with open(trgt_path + f"{i + 1}.npy", 'wb') as out:
                 np.save(out, nd_arrs[i])
 
 
-dir_folder_path = "/mnt/storage/datasets/4DCT/041516 New Cases"
-dir_folder_path += "/2/Anonymized - 3863450"
-dir_folder_path += "/Ctacoc/CorVein_Bi 1.5 B25f MPR 0-95% Matrix 256 - 9/"
-patient_num = "2"
-first_img_number = 8
-last_img_number = 27
+dir_folder_path = "/mnt/storage/datasets/4DCT/041516 New Cases/30/Anonymized - 4435086/Ctacoc/DS_CorCTABi 1.5 B26f 0-95% - 8/"
+target_path="/mnt/storage/datasets/4DCT/041516 New Cases/Nd_arrays/"
+patient_num = "30"
+first_img_number = 7
+last_img_number = 26
 
-convert_matrix_folder_to_3dnpy_files(
-    dir_folder_path, patient_num, first_img_number, last_img_number
-)
+convert_matrix_folder_to_3dnpy_files(dir_folder_path,target_path, patient_num, first_img_number, last_img_number)
