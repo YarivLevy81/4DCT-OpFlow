e8fba15dad1a54603ed5762a571eafe1fa17b014
Yariv Levy <Yarivlevy81@gmail.com>
2020-12-05 19:13:37 +0200

    Addition to 3d PWC

--- models/pwc3d.py
+++ models/pwc3d.py
@@ -12,15 +12,179 @@ def get_model(args):
     return model
 
 class PWC3d_Lite(nn.Module):
-    def __init__(self, args):
-        pass
+    def __init__(self, args, upsample=True, reduce_dens=False, search_range=4):
+        self.search_range = search_range
+        #TODO: num_chs starts from 1 because grayscale?
+        self.num_chs = [1, 16, 32, 64, 96, 128, 192]
+        self.output_level = 4
+        self.num_levels = 7
+        self.leakyRELU = nn.LeakyReLU(0.1, inplace=True)
+
+        self.feature_pyramid_extractor = FeatureExtractor(self.num_chs)
+
+        self.upsample = upsample
+        self.reduce_dense = reduce_dens
+
+        self.corr = Correlation(pad_size=self.search_range, kernel_size=1,
+                                max_displacement=self.search_range, stride1=1,
+                                stride2=1, corr_multiply=1)
+
+        self.dim_corr = (self.search_range * 2 + 1) ** 2
+        self.num_ch_in = 32 + (self.dim_corr + 2) * (self.n_frames - 1)
+
+        if self.reduce_dense:
+            self.flow_estimators = FlowEstimatorReduce(self.num_ch_in)
+        else:
+            self.flow_estimators = FlowEstimatorDense(self.num_ch_in)
+
+        self.context_networks = ContextNetwork(
+            (self.flow_estimators.feat_dim + 2) * (self.n_frames - 1))
+
+        self.conv_1x1 = nn.ModuleList([conv(192, 32, kernel_size=1, stride=1, dilation=1),
+                                       conv(128, 32, kernel_size=1, stride=1, dilation=1),
+                                       conv(96, 32, kernel_size=1, stride=1, dilation=1),
+                                       conv(64, 32, kernel_size=1, stride=1, dilation=1),
+                                       conv(32, 32, kernel_size=1, stride=1, dilation=1)]
+
+    def num_parameters(self):
+        return sum(
+            [p.data.nelement() if p.requires_grad else 0 for p in self.parameters()])
+
+    def init_weights(self):
+        for layer in self.named_modules():
+            if isinstance(layer, nn.Conv3d):
+                nn.init.kaiming_normal_(layer.weight)
+                if layer.bias is not None:
+                    nn.init.constant_(layer.bias, 0)
+
+            elif isinstance(layer, nn.ConvTranspose3d):
+                nn.init.kaiming_normal_(layer.weight)
+                if layer.bias is not None:
+                    nn.init.constant_(layer.bias, 0)
 
     def forward(self, x1_pyramid, x2_pyramid):
+
+        # outputs
+        flows = []
+
+        #
+
         return x1_pyramid
 
+
+class Correlation(nn.Module):
+    def __init__(self, max_displacement=4, *args, **kwargs):
+        super(Correlation, self).__init__()
+        self.max_displacement = max_displacement
+        self.output_dim = 2 * self.max_displacement + 1
+        self.pad_size = self.max_displacement
+
+    def forward(self, x1, x2):
+        B, C, H, W = x1.size()
+
+        x2 = F.pad(x2, [self.pad_size] * 4)
+        cv = []
+        for i in range(self.output_dim):
+            for j in range(self.output_dim):
+                cost = x1 * x2[:, :, i:(i + H), j:(j + W)]
+                cost = torch.mean(cost, 1, keepdim=True)
+                cv.append(cost)
+        return torch.cat(cv, 1)
+
+
+def conv(in_planes, out_planes, kernel_size=3, stride=1, dilation=1, isReLU=True):
+    if isReLU:
+        return nn.Sequential(
+            nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                      dilation=dilation,
+                      padding=((kernel_size - 1) * dilation) // 2, bias=True),
+            nn.LeakyReLU(0.1, inplace=True)
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                      dilation=dilation,
+                      padding=((kernel_size - 1) * dilation) // 2, bias=True)
+        )
+
+
 class ContextNetwork(nn.Module):
-    def __init__(self, x):
-        pass
+    def __init__(self, ch_in):
+        super(ContextNetwork, self).__init__()
+
+        self.convs = nn.Sequential(
+            conv(ch_in, 128, 3, 1, 1),
+            conv(128, 128, 3, 1, 2),
+            conv(128, 128, 3, 1, 4),
+            conv(128, 96, 3, 1, 8),
+            conv(96, 64, 3, 1, 16),
+            conv(64, 32, 3, 1, 1),
+            conv(32, 2, isReLU=False)
+        )
 
     def forward(self, x):
-        pass
+        return self.convs(x)
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, num_chs):
+        super(FeatureExtractor, self).__init__()
+        self.num_chs = num_chs
+        self.convs = nn.ModuleList()
+
+        for l, (ch_in, ch_out) in enumerate(zip(num_chs[:-1], num_chs[1:])):
+            layer = nn.Sequential(
+                conv(ch_in, ch_out, stride=2),
+                conv(ch_out, ch_out)
+            )
+            self.convs.append(layer)
+
+    def forward(self, x):
+        feature_pyramid = []
+        for conv in self.convs:
+            x = conv(x)
+            feature_pyramid.append(x)
+
+        return feature_pyramid[::-1]
+
+class FlowEstimatorDense(nn.Module):
+    def __init__(self, ch_in):
+        super(FlowEstimatorDense, self).__init__()
+        self.conv1 = conv(ch_in, 128)
+        self.conv2 = conv(ch_in + 128, 128)
+        self.conv3 = conv(ch_in + 256, 96)
+        self.conv4 = conv(ch_in + 352, 64)
+        self.conv5 = conv(ch_in + 416, 32)
+        self.feat_dim = ch_in + 448
+        self.conv_last = conv(ch_in + 448, 2, isReLU=False)
+
+    def forward(self, x):
+        x1 = torch.cat([self.conv1(x), x], dim=1)
+        x2 = torch.cat([self.conv2(x1), x1], dim=1)
+        x3 = torch.cat([self.conv3(x2), x2], dim=1)
+        x4 = torch.cat([self.conv4(x3), x3], dim=1)
+        x5 = torch.cat([self.conv5(x4), x4], dim=1)
+        x_out = self.conv_last(x5)
+        return x5, x_out
+
+
+class FlowEstimatorReduce(nn.Module):
+    # can reduce 25% of training time.
+    def __init__(self, ch_in):
+        super(FlowEstimatorReduce, self).__init__()
+        self.conv1 = conv(ch_in, 128)
+        self.conv2 = conv(128, 128)
+        self.conv3 = conv(128 + 128, 96)
+        self.conv4 = conv(128 + 96, 64)
+        self.conv5 = conv(96 + 64, 32)
+        self.feat_dim = 32
+        self.predict_flow = conv(64 + 32, 2, isReLU=False)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(torch.cat([x1, x2], dim=1))
+        x4 = self.conv4(torch.cat([x2, x3], dim=1))
+        x5 = self.conv5(torch.cat([x3, x4], dim=1))
+        flow = self.predict_flow(torch.cat([x4, x5], dim=1))
+        return x5, flow
