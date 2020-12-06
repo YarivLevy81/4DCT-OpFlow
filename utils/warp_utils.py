0432de135db10806a8642b9e50c6837a6a611152
doronkopit <doronkopit@mail.tau.ac.il>
2020-12-06 17:01:19 +0200

    warp utils

--- utils/warp_utils.py
+++ utils/warp_utils.py
@@ -4,5 +4,32 @@ import torch.nn.functional as F
 import inspect
 
 
-def flow_warp(x):
-    pass
+def mesh_grid(B, H, W, D):
+    # batches not implented
+    x = torch.arange(H)
+    y = torch.arange(W)
+    z = torch.arange(D)
+    return torch.stack(torch.meshgrid(x, y, z), 1)
+
+
+def norm_grid(v_grid):
+    _, _, H, W, D = v_grid.size()
+
+    # scale grid to [-1,1]
+    v_grid_norm = torch.zeros_like(v_grid)
+    v_grid_norm[:, 0, :, :] = 2.0 * v_grid[:, 0, :, :] / (W - 1) - 1.0
+    v_grid_norm[:, 1, :, :] = 2.0 * v_grid[:, 1, :, :] / (H - 1) - 1.0
+    v_grid_norm[:, 2, :, :] = 2.0 * v_grid[:, 2, :, :] / (D - 1) - 1.0
+    # may need to permute flow to fit to grid
+    return v_grid_norm
+
+
+def flow_warp(x, flow12, pad='border', mode='bilinear'):
+    B, _, H, W, D = x.size()
+
+    base_grid = mesh_grid(B, H, W).type_as(x)  # B2HW
+
+    v_grid = norm_grid(base_grid + flow12)  # BHW2
+    im1_recons = nn.functional.grid_sample(x, v_grid, mode=mode, padding_mode=pad, align_corners=True)
+
+    return im1_recons
