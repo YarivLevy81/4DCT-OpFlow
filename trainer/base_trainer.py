bce1aef6010fd193c3887f0d1e99d0dab0a52484
Yariv Levy <Yarivlevy81@gmail.com>
2020-12-06 16:56:10 +0200

    More progress with trainer, need to implement loss fucntions

--- trainer/base_trainer.py
+++ trainer/base_trainer.py
@@ -1,7 +1,8 @@
 import torch
 import numpy as np
 from abc import abstractmethod
-
+from utils.torch_utils import bias_parameters, weight_parameters, \
+    load_checkpoint, save_checkpoint
 
 class BaseTrainer:
     """
@@ -11,18 +12,79 @@ class BaseTrainer:
     def __init__(self, train_loader, valid_loader, model, loss_func, args):
         self.train_loader = train_loader
         self.valid_loader = valid_loader
-        self.model = model
-        self.loss_func = loss_func
+        self.device = self._init_device()
         self.args = args
+        
+        self.model = self._init_model(model)
+        self.optimizer = self._get_optimizer()
+        self.loss_func = loss_func
 
-    @abstractmethod
-    def _run_one_epoch(self):
-        ...
+        self.best_error = np.inf
+        self.i_epoch = 0
+        self.i_iter = 0
 
     def train(self):
         for epoch in range(self.args.epochs):
             self._run_one_epoch()
 
-            if epoch % self.args.log_interval == 0:
-                print(' * Epoch {} '.format(epoch))
+            if self.i_epoch % self.args.log_interval == 0:
+                errors, error_names = self._validate()
+                valid_res = ' '.join(
+                    '{}: {:.2f}'.format(*t) for t in zip(error_names, errors))
+                self._log.info(' * Epoch {} '.format(self.i_epoch) + valid_res)
 
+    @abstractmethod
+    def _run_one_epoch(self):
+        ...
+
+    @abstractmethod
+    def _validate(self):
+        ...
+    
+    @abstractmethod
+    def _get_optimizer(self):
+        param_groups = [
+            {'params': bias_parameters(self.model.module),
+             'weight_decay': 0},
+            {'params': weight_parameters(self.model.module),
+             'weight_decay': 1e-6}]
+
+        return torch.optim.Adam(param_groups, self.args.lr, 
+                                betas=(0.9, 0.999), eps=1e-7)
+
+    def _init_model(self, model):
+        model = model.to(self.device)
+        if self.args.pretrained_model:
+            epoch, weights = load_checkpoint(self.args.pretrained_model)
+
+            from collections import OrderedDict
+            new_weights = OrderedDict()
+            model_keys = list(model.state_dict().keys())
+            weight_keys = list(weights.keys())
+            for a, b in zip(model_keys, weight_keys):
+                new_weights[a] = weights[b]
+            weights = new_weights
+            model.load_state_dict(weights)
+        
+        else:
+            print("=> Train from scratch")
+            model.init_weights()
+        
+        # model = torch.nn.DataParallel(model, device_ids=self.device_ids)
+        return model
+
+
+    def _init_device(self):
+        # TODO: implement with cuda also
+        return torch.device('cpu')
+
+    def save_model(self, error, name):
+        is_best = error < self.best_error
+
+        if is_best:
+            self.best_error = error
+
+        models = {'epoch': self.i_epoch,
+                  'state_dict': self.model.module.state_dict()}
+
+        save_checkpoint(self.save_root, models, name, is_best)
---
+++ utils/torch_utils.py
@@ -0,0 +1,78 @@
+import torch
+import shutil
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import numbers
+import random
+import math
+
+
+def init_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def weight_parameters(module):
+    return [param for name, param in module.named_parameters() if 'weight' in name]
+
+
+def bias_parameters(module):
+    return [param for name, param in module.named_parameters() if 'bias' in name]
+
+
+def load_checkpoint(model_path):
+    weights = torch.load(model_path, map_location={'cuda:0': 'cpu'})
+    epoch = None
+    if 'epoch' in weights:
+        epoch = weights.pop('epoch')
+    if 'state_dict' in weights:
+        state_dict = (weights['state_dict'])
+    else:
+        state_dict = weights
+    return epoch, state_dict
+
+
+def save_checkpoint(save_path, states, file_prefixes, is_best, filename='ckpt.pth.tar'):
+    def run_one_sample(save_path, state, prefix, is_best, filename):
+        torch.save(state, save_path / '{}_{}'.format(prefix, filename))
+        if is_best:
+            shutil.copyfile(save_path / '{}_{}'.format(prefix, filename),
+                            save_path / '{}_model_best.pth.tar'.format(prefix))
+
+    if not isinstance(file_prefixes, str):
+        for (prefix, state) in zip(file_prefixes, states):
+            run_one_sample(save_path, state, prefix, is_best, filename)
+
+    else:
+        run_one_sample(save_path, states, file_prefixes, is_best, filename)
+
+
+def restore_model(model, pretrained_file):
+    epoch, weights = load_checkpoint(pretrained_file)
+
+    model_keys = set(model.state_dict().keys())
+    weight_keys = set(weights.keys())
+
+    # load weights by name
+    weights_not_in_model = sorted(list(weight_keys - model_keys))
+    model_not_in_weights = sorted(list(model_keys - weight_keys))
+    if len(model_not_in_weights):
+        print('Warning: There are weights in model but not in pre-trained.')
+        for key in (model_not_in_weights):
+            print(key)
+            weights[key] = model.state_dict()[key]
+    if len(weights_not_in_model):
+        print('Warning: There are pre-trained weights not in model.')
+        for key in (weights_not_in_model):
+            print(key)
+        from collections import OrderedDict
+        new_weights = OrderedDict()
+        for key in model_keys:
+            new_weights[key] = weights[key]
+        weights = new_weights
+
+    model.load_state_dict(weights)
+    return model
