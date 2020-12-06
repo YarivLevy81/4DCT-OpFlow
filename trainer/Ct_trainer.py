08aca53f3b44d6541846e348ddf7940b7ea22137
Yariv Levy <Yarivlevy81@gmail.com>
2020-12-06 17:42:22 +0200

    Working on trainer

--- trainer/Ct_trainer.py
+++ trainer/Ct_trainer.py
@@ -2,7 +2,7 @@ import time
 import torch
 import numpy as np
 from .base_trainer import BaseTrainer
-
+from utils.warp_utils import flow_warp
 
 class TrainFramework(BaseTrainer):
     def __init__(self, train_loader, valid_loader, model, loss_func, args):
@@ -18,7 +18,11 @@ class TrainFramework(BaseTrainer):
 
             img1, img2 = data
             res = self.model(img1, img2)
-        
+            
+            self.optimizer.zero_grad()
+            loss = 0
+
+
 
     def _validate(self):
         pass
